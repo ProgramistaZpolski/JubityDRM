20df2f31a98f6d5e44750108283707dc1654de31
ProgramistaZpolski <37271169+ProgramistaZpolski@users.noreply.github.com>
2019-05-04 22:34:26 +0200

    Rename main.cs to DRM.cs

---
+++ DRM.cs
@@ -0,0 +1,6 @@
+Process.Start("https://www.youtube.com/watch?v=up863eQKGUI");
+                Process.Start("https://www.youtube.com/watch?v=386zfdJt8JM");
+                Process.Start("https://www.youtube.com/watch?v=hUCyvw4w_yk");
+                Process.Start("https://www.youtube.com/watch?v=BLtWyp-tbhk");
+                MessageBox.Show("Ponieważ wywaliłeś logo jubity z plików masz 2  " +
+                    "opcje: 1) przez 10 godzin słuchać earrape dont coppy that floppy 2) przywrócić plik");
